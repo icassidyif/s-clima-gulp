dfe82f9c2d3e018d79232ba7e916c5c30327fa9c
Ihor <icassidyif@gmail.com>
2020-08-29 22:39:16 +0300

    parallax done beta2

--- app/js/particles/parallax.js
+++ app/js/particles/parallax.js
@@ -4,7 +4,6 @@ function parallaxEffect(event) {
     layer.style.transform = `translate(${event.clientX * speed / 1000}px, ${event.clientY * speed / 1300}px )`;
   })
 }
-
 let parallax = document.querySelector('.parallax');
 let layers = parallax.querySelectorAll('.parallax__layer');
 parallax.addEventListener('mousemove', parallaxEffect);
