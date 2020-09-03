f723d1018cf8ecb5034b4909ebebfc11a6563359
Ihor <icassidyif@gmail.com>
2020-09-03 19:22:00 +0300

    banners section

--- s-clima-gulp/js/app.js
+++ s-clima-gulp/js/app.js
@@ -464,6 +464,44 @@ $(document).ready(function () {
       }
     }]
   });
+  $('.slider-banners').slick({
+    arrows: true,
+    dots: false,
+    adaptiveHeight: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    speed: 500,
+    easing: 'ease',
+    infinite: true,
+    initialSlide: 1,
+    autoplay: true,
+    autoplaySpeed: 3500,
+    pauseOnFocus: true,
+    pauseOnHover: true,
+    pauseOnDotsHover: true,
+    draggable: true,
+    swipe: true,
+    touchThreshold: 5,
+    touchMove: true,
+    waitForAnimate: true,
+    centerMode: false,
+    variableWidth: false,
+    lazyLoad: 'ondemand',
+    responsive: [{
+      breakpoint: 992,
+      settings: {// slidesToShow: 2
+      }
+    }, {
+      breakpoint: 768,
+      settings: {// slidesToShow: 1
+      }
+    }, {
+      breakpoint: 576,
+      settings: {// slidesToShow: 1,
+        // arrows: false
+      }
+    }]
+  });
 }); //Spoiler=========================================================================
 // $('.spoiler').click(function(){
 //   $(this).toggleClass('opened').toggleClass('closed').prev().slideToggle(700);
